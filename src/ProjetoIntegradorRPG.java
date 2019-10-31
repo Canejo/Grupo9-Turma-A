901523eae740980d02e2cf0b33f85c96ea651ac0
Lsantiago19 <55859303+Lsantiago19@users.noreply.github.com>
2019-10-31 20:29:21 -0300

    random de 1 a 20

--- src/ProjetoIntegradorRPG.java
+++ src/ProjetoIntegradorRPG.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.util.Random;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,45 +13,46 @@ import java.util.Scanner;
  * @author felipe.acanejo
  */
 public class ProjetoIntegradorRPG {
+
     static Scanner input = new Scanner(System.in);
-    static String[] enredo = new String[] {
+    static String[] enredo = new String[]{
         "",
         ""
     };
-    
+
     static void novoJogo() {
         
     }
-    
+
     static void comoJogar() {
         System.out.println("Aperte qualquer tecla para voltar...");
         input.next();
-        exibirMenu();   
+        exibirMenu();
     }
-    
+
     static void creditos() {
         System.out.println("Aperte qualquer tecla para voltar...");
         input.next();
         exibirMenu();
     }
-    
+
     static void exibirMenu() {
         int opcaoMenu = 0;
-        
-        do{
+
+        do {
             System.out.println("1) Novo jogo");
             System.out.println("2) Como jogar");
             System.out.println("3) Créditos");
             System.out.println("4) Sair");
             System.out.print("Digite uma opção para continuar: ");
             opcaoMenu = input.nextInt();
-        } while(opcaoMenu <= 0 || opcaoMenu > 4);
-        
+        } while (opcaoMenu <= 0 || opcaoMenu > 4);
+
         selecionarMenu(opcaoMenu);
     }
-    
+
     static void selecionarMenu(int opcaoMenu) {
-        switch(opcaoMenu) {
+        switch (opcaoMenu) {
             case 1:
                 novoJogo();
                 break;
@@ -62,8 +64,15 @@ public class ProjetoIntegradorRPG {
                 break;
         }
     }
-    
+
+    static int de1a20Aleat() {
+        Random geraNum = new Random();
+        int num = 1 + geraNum.nextInt(20);
+        return num;
+    }
+
     public static void main(String[] args) {
-        exibirMenu();   
+        System.out.println(de1a20Aleat());
+        exibirMenu();
     }
 }
