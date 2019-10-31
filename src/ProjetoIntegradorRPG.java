48c23939cd6ccf4cd36e703d2890c8ea16dd3f86
Canejo <felipecanejo@gmail.com>
2019-10-31 19:53:37 -0300

    Opções do menu

--- src/ProjetoIntegradorRPG.java
+++ src/ProjetoIntegradorRPG.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,7 +12,58 @@
  * @author felipe.acanejo
  */
 public class ProjetoIntegradorRPG {
+    static Scanner input = new Scanner(System.in);
+    static String[] enredo = new String[] {
+        "",
+        ""
+    };
+    
+    static void novoJogo() {
+        
+    }
+    
+    static void comoJogar() {
+        System.out.println("Aperte qualquer tecla para voltar...");
+        input.next();
+        exibirMenu();   
+    }
+    
+    static void creditos() {
+        System.out.println("Aperte qualquer tecla para voltar...");
+        input.next();
+        exibirMenu();
+    }
+    
+    static void exibirMenu() {
+        int opcaoMenu = 0;
+        
+        do{
+            System.out.println("1) Novo jogo");
+            System.out.println("2) Como jogar");
+            System.out.println("3) Créditos");
+            System.out.println("4) Sair");
+            System.out.print("Digite uma opção para continuar: ");
+            opcaoMenu = input.nextInt();
+        } while(opcaoMenu <= 0 || opcaoMenu > 4);
+        
+        selecionarMenu(opcaoMenu);
+    }
+    
+    static void selecionarMenu(int opcaoMenu) {
+        switch(opcaoMenu) {
+            case 1:
+                novoJogo();
+                break;
+            case 2:
+                comoJogar();
+                break;
+            case 3:
+                creditos();
+                break;
+        }
+    }
+    
     public static void main(String[] args) {
-        System.out.println("a");
+        exibirMenu();   
     }
 }
