3ab3bd2e6cee610f93f30d1191c57bc740a2f930
jardeljj <55859324+jardeljj@users.noreply.github.com>
2019-10-31 21:35:40 -0300

    Retorno de opção

--- src/ProjetoIntegradorRPG.java
+++ src/ProjetoIntegradorRPG.java
@@ -6,51 +6,51 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author felipe.acanejo
  */
 public class ProjetoIntegradorRPG {
+
     static Scanner input = new Scanner(System.in);
-    static String[] enredo = new String[] {
+    static String[] enredo = new String[]{
         "",
         ""
     };
-    
+
     static void novoJogo() {
-        
+
     }
-    
+
     static void comoJogar() {
         System.out.println("Aperte qualquer tecla para voltar...");
         input.next();
-        exibirMenu();   
+        exibirMenu();
     }
-    
+
     static void creditos() {
         System.out.println("Aperte qualquer tecla para voltar...");
         input.next();
         exibirMenu();
     }
-    
+
     static void exibirMenu() {
         int opcaoMenu = 0;
-        
-        do{
+
+        do {
             System.out.println("1) Novo jogo");
             System.out.println("2) Como jogar");
             System.out.println("3) Créditos");
             System.out.println("4) Sair");
             System.out.print("Digite uma opção para continuar: ");
             opcaoMenu = input.nextInt();
-        } while(opcaoMenu <= 0 || opcaoMenu > 4);
-        
+        } while (opcaoMenu <= 0 || opcaoMenu > 4);
+
         selecionarMenu(opcaoMenu);
     }
-    
+
     static void selecionarMenu(int opcaoMenu) {
-        switch(opcaoMenu) {
+        switch (opcaoMenu) {
             case 1:
                 novoJogo();
                 break;
@@ -62,8 +62,35 @@ public class ProjetoIntegradorRPG {
                 break;
         }
     }
-    
+
+    static int escolha() {
+        int opcao = 0;
+        do {
+        char escolhaDoUsuario = input.next().charAt(0);
+        
+        
+            switch (escolhaDoUsuario) {
+
+                case 's':
+                    opcao = 1;
+                    break;
+                case 'n':
+                    opcao = 2;
+                    break;
+                case 'd':
+                    opcao = 3;
+                    break;
+
+            }
+            System.out.println(opcao);
+            
+        }while(opcao == 0);
+        return opcao;
+    }
+
     public static void main(String[] args) {
-        exibirMenu();   
+        escolha();
+        
+        exibirMenu();
     }
 }
